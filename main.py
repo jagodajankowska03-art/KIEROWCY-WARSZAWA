b2a20d62bf2d822cdba1e6b8a21b1e5844faaf7d
jagodajankowska03-art <jagodajankowska03@gmail.com>
2026-01-12 11:18:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Debiut – kierowcy działa :truck:"
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
